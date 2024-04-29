53bf4ce5ce789c52d0862441be02b76fe16c8357
NourhanAli10 <nourhan.ali.rezk92@gmail.com>
2024-04-29 19:37:04 +0300

    task 0

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,26 @@
+function getListStudents() {
+  const arrayOfObjects = [];
+  const SanFrancisco = {
+    id: 1,
+    firstName: 'Guillaume',
+    location: 'San Francisco',
+  };
+  const Columbia = {
+    id: 2,
+    firstName: 'James',
+    location: 'Columbia',
+  };
+  const Serena = {
+    id: 3,
+    firstName: 'Serena',
+    location: 'San Francisco',
+  };
+
+  arrayOfObjects[0] = [SanFrancisco];
+  arrayOfObjects[1] = [Columbia];
+  arrayOfObjects[2] = [Serena];
+
+  return arrayOfObjects;
+}
+
+export default getListStudents;

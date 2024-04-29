13fdf30b05d1c0e6ea7859fcb597d516cb69258a
NourhanAli10 <nourhan.ali.rezk92@gmail.com>
2024-04-29 20:22:25 +0300

    task0

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,27 +1,19 @@
-/* eslint-disable */
-function getListStudents() {
-  const arrayOfObjects = [];
-  const SanFrancisco = {
+const getListStudents = () => [
+  {
     id: 1,
     firstName: 'Guillaume',
     location: 'San Francisco',
-  };
-  const Columbia = {
+  },
+  {
     id: 2,
     firstName: 'James',
     location: 'Columbia',
-  };
-  const Serena = {
+  },
+  {
     id: 5,
     firstName: 'Serena',
     location: 'San Francisco',
-  };
-
-  arrayOfObjects[0] = SanFrancisco;
-  arrayOfObjects[1] = Columbia;
-  arrayOfObjects[2] = Serena;
-
-  return arrayOfObjects;
-}
+  },
+];
 
 export default getListStudents;

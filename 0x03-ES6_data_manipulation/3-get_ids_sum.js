4f16f8da9d760c8aa8e4cb2243314a5b3ba38552
NourhanAli10 <nourhan.ali.rezk92@gmail.com>
2024-05-04 00:57:54 +0300

    task 2 , 3

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -2,6 +2,6 @@ export default function getStudentIdsSum(students) {
   const initialValue = 0;
   return students.reduce(
     (accumulator, currentValue) => accumulator + currentValue.id,
-    initialValue
+    initialValue,
   );
 }

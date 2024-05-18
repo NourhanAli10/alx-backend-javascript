73286c4d0a1d9331db939d8a9692a6ea421bf957
NourhanAli10 <nourhan.ali.rezk92@gmail.com>
2024-05-19 00:39:10 +0300

    task 9

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -1,46 +1,37 @@
-// eslint-disable-next-line max-classes-per-file
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable max-classes-per-file */
 export class HolbertonClass {
   constructor(year, location) {
-    // eslint-disable-next-line no-underscore-dangle
     this._year = year;
-    // eslint-disable-next-line no-underscore-dangle
     this._location = location;
   }
 
   get year() {
-    // eslint-disable-next-line no-underscore-dangle
     return this._year;
   }
 
   get location() {
-    // eslint-disable-next-line no-underscore-dangle
     return this._location;
   }
 }
 
 export class StudentHolberton {
   constructor(firstName, lastName, holbertonClass) {
-    // eslint-disable-next-line no-underscore-dangle
     this._firstName = firstName;
-    // eslint-disable-next-line no-underscore-dangle
     this._lastName = lastName;
-    // eslint-disable-next-line no-underscore-dangle
     this._holbertonClass = holbertonClass;
   }
 
   get fullName() {
-    // eslint-disable-next-line no-underscore-dangle
     return `${this._firstName} ${this._lastName}`;
   }
 
   get holbertonClass() {
-    // eslint-disable-next-line no-underscore-dangle
     return this._holbertonClass;
   }
 
   get fullStudentDescription() {
-    // eslint-disable-next-line no-underscore-dangle
-    return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
+    return `${this.fullName} - ${this.holbertonClass.year} - ${this.holbertonClass.location}`;
   }
 }
 
@@ -53,11 +44,5 @@ const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
 const student4 = new StudentHolberton('Donald', 'Bush', class2019);
 const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 
-// Exporting list of students
-export const listOfStudents = [
-  student1,
-  student2,
-  student3,
-  student4,
-  student5,
-];
+const listOfStudents = [student1, student2, student3, student4, student5];
+export default listOfStudents;

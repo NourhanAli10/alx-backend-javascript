080edd8a1fcbe59e11f0c88fe092f7fe9d42d693
NourhanAli10 <nourhan.ali.rezk92@gmail.com>
2024-05-19 00:10:41 +0300

    task 6

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,6 +1,6 @@
 import Building from './5-building';
 
-export default class SkyHighBuilding extends Building {
+class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
     // eslint-disable-next-line no-underscore-dangle
@@ -12,16 +12,10 @@ export default class SkyHighBuilding extends Building {
     return this._floors;
   }
 
-  set floors(value) {
-    if (typeof floors !== 'number') {
-      throw new TypeError('Floors must be a number');
-    }
-    // eslint-disable-next-line no-underscore-dangle
-    this._floors = value;
-  }
-
   evacuationWarningMessage() {
     // eslint-disable-next-line no-underscore-dangle
-    return `Evacuate slowly the ${this._floors} floors.`;
+    return `Evacuate slowly the ${this._floors} floors`;
   }
 }
+
+export default SkyHighBuilding;

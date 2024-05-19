18810ab389f6c8e4e31ec8c1d6a09ae5216dfe46
NourhanAli10 <nourhan.ali.rezk92@gmail.com>
2024-05-20 00:34:08 +0300

    task 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,14 +1,12 @@
-// eslint-disable-next-line import/no-unresolved
 import { uploadPhoto, createUser } from './utils';
 
-function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
-    .then(([photoResponse, userResponse]) => {
-      console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
-    })
-    .catch(() => {
-      console.log('Signup system offline');
-    });
+async function handleProfileSignup() {
+  try {
+    const [photoResponse, userResponse] = await Promise.all([uploadPhoto(), createUser()]);
+    console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
+  } catch (error) {
+    console.log('Signup system offline');
+  }
 }
 
 export default handleProfileSignup;
